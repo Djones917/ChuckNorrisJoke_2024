a4c1b398e7ffc74abb4b9ba1cb6c1a8ebbe57c54
Derek <nightwing917@hotmail.com>
2024-07-27 19:58:21 -0600

    testing eventlistener

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('joke-btn');
 
+const getJoke = () => {
+    console.log(1,2,3);
+}
+
 jokeBtn.addEventListener('click', getJoke);
