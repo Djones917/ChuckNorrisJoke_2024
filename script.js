58248543268940b467bb60b65e716681635cd30a
Derek <nightwing917@hotmail.com>
2024-08-02 19:55:46 -0600

    adding innerHTML

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const jokeBtn = document.getElementById('joke-btn');
 
 const getJoke = () => {
     const xhr = XMLHttpRequest();
+
     xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+         if (this.status === 200) {
+            jokeEl.innerHTML = JSON.parse(this.responseText);
+         } else {
+
+         }
+      }
+    }
 }
 
 jokeBtn.addEventListener('click', getJoke);
