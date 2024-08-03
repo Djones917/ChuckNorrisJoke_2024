02a104bf54511fdcd18646b177cb5801a3035bf5
Derek <nightwing917@hotmail.com>
2024-08-02 19:46:31 -0600

    added get chuck request

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const jokeBtn = document.getElementById('joke-btn');
 
 const getJoke = () => {
     const xhr = XMLHttpRequest();
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 }
 
 jokeBtn.addEventListener('click', getJoke);
